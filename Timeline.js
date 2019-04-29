a6bff948cbeabd673b6b073159acc61c2d078d5a
payedz <farid.z_21129@utp.edu.my>
2019-04-29 14:49:32 +0800

    Update Timeline.js

--- Timeline.js
+++ Timeline.js
@@ -1,98 +1,12 @@
-$(".dot:nth-child(1)").click(function() {
-  $(".inside").animate(
-    {
-      width: "20%"
-    },
-    500
-  );
-});
-$(".dot:nth-child(2)").click(function() {
-  $(".inside").animate(
-    {
-      width: "40%"
-    },
-    500
-  );
-});
-$(".dot:nth-child(3)").click(function() {
-  $(".inside").animate(
-    {
-      width: "60%"
-    },
-    500
-  );
-});
-$(".dot:nth-child(4)").click(function() {
-  $(".inside").animate(
-    {
-      width: "80%"
-    },
-    500
-  );
-});
-if ($("#switch1").not(":checked")) {
-  $(".modal").wrap('<div class="mask"></div>');
-  $(".mask").click(function() {
-    $(this).fadeOut(300);
-    $(".mask article").animate(
-      {
-        top: "-100%"
-      },
-      300
-    );
-  });
-
-  $(".dot").click(function() {
-    var modal = $(this).attr("id");
-    $(".mask")
-      .has("article." + modal)
-      .fadeIn(300);
-    $(".mask article." + modal)
-      .fadeIn(0)
-      .animate(
-      {
-        top: "10%"
-      },
-      300
-    );
-  });
-}
-$("#switch1").click(function() {
-  if ($("#switch1").is(":checked")) {
-    $(".modal").unwrap('<div class="mask"></div>');
-    $(".modal").hide();
-    $(".modal").addClass("nobox");
-    $(".dot").click(function() {
-      var modal = $(this).attr("id");
-      $("article.nobox").hide();
-      $("article.nobox." + modal).fadeIn(200);
-    });
-  } else {
-    $("article").removeClass("nobox");
-    $(".modal").wrap('<div class="mask"></div>');
-    $(".mask").click(function() {
-      $(this).fadeOut(300);
-      $(".mask article").animate(
-        {
-          top: "-100%"
-        },
-        300
-      );
-    });
-
-    $(".dot").click(function() {
-      var modal = $(this).attr("id");
-      $(".mask")
-        .has("article." + modal)
-        .fadeIn(300);
-      $(".mask article." + modal)
-        .fadeIn(0)
-        .animate(
-        {
-          top: "10%"
-        },
-        300
-      );
-    });
-  }
+$(function(){
+	var inputs = $('.input');
+	var paras = $('.description-flex-container').find('p');
+	$(inputs).click(function(){
+		var t = $(this),
+				ind = t.index(),
+				matchedPara = $(paras).eq(ind);
+		
+		$(t).add(matchedPara).addClass('active');
+		$(inputs).not(t).add($(paras).not(matchedPara)).removeClass('active');
+	});
 });
